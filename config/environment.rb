dea98a03255eaeef167fb952dc84e60253f39f1e
Oleksii Sliepov <slepovaleksei09@gmail.com>
2014-09-03 00:21:11 +0300

    Add email-send, templeate e-mail, and functional tests

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,16 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-Rails.application.initialize!
+Rails.application.initialize! do
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.smtp_settings = {
+		:address => "smtp.gmail.com",
+		:port => 587,
+		:domain => "google.com",
+		:authentication =>"plain",
+		:user_name => "thangtuan8@gmail.com",
+		:password => "78789878sa",
+	}
+
+end
+
